0df533de28ba05ddf5f4ebb741a4859d7abad19c
majaliju <majalijunyc@gmail.com>
2022-11-06 00:52:40 -0400

    updating; finishing tomorrow morning

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
+  ## gotta figure out details of this first part
+  ## add "and " to the end of the last one
 
+  puts array
+  ## join array elments, seperated by ". "
 end
+
+
+oxford_comma(%w[fiddleheads okra kohlrabi])
