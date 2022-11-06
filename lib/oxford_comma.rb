a2e42a241ceff1dbf5bd8869c77d72464342ca82
majaliju <majalijunyc@gmail.com>
2022-11-06 13:57:08 -0500

    completed

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,14 @@
 def oxford_comma(array)
-  ## gotta figure out details of this first part
-  ## add "and " to the end of the last one
-
-  puts array
-  ## join array elments, seperated by ". "
+  puts array.length
+  if array.length >= 3
+    fixedString = array[-1].insert(0, 'and ')
+    array[-1] = fixedString
+    fixedArray = array.join(", ")
+    return fixedArray
+  elsif array.length == 2
+    fixedArray = array.join(" and ")
+    return fixedArray
+  else array.length == 1
+    return array.join("")
+  end
 end
-
-
-oxford_comma(%w[fiddleheads okra kohlrabi])
